8d4980a8ec48d1e34a3174797573e92c0de69771
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 17:23:56 -0400

    1st projet
    Did the input for prompts and added the sum array.

--- Week 2 Homework/js/script.js
+++ Week 2 Homework/js/script.js
@@ -2,4 +2,16 @@
 WPF Section 101
 Assignment: Expressions
  */
-alert("Testing 1,2,3");
+//alert("Testing 1,2,3");
+
+//Create a Js file that will determine what your average weight loss is.
+// In order to find an average, first we must get the input from the prompts.
+var week1 = prompt("How much did you loose in week 1");
+var week2 = prompt("How much did you loose in week 2");
+var week3 = prompt("How much did you loose in week 3");
+var week4 = prompt("How much did you loose in week 4");
+
+//Now we need to add all of the information that was given together to get a sum.
+var sum = [week1, week2, week3, week4];
+//Now to get the total.
+var total = sum[0] +sum[1] +sum[2] +sum[3]; 
