8a74432b05fc5e7c96c42163b9a308042257c81a
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 17:50:08 -0400

    1 project
    Found why its going wrong. It is being stringed with out “” why?

--- Week 2 Homework/js/script.js
+++ Week 2 Homework/js/script.js
@@ -14,9 +14,11 @@ var week4 = prompt("How much did you loose in week 4");
 //Now we need to add all of the information that was given together to get a sum.
 var sum = [week1, week2, week3, week4];
 // test to see if the answer is correct.
-console.log(sum); 
+console.log(sum);
 //Now to get the total.
 var total = sum[0] + sum[1] + sum[2] + sum[3];
+//Test to see if this answer is right.
+console.log(total); 
 // In order to find the average you will need to divide by the number of prompts that were given.
 var average = total / 4;
 // In order for the person to see what the average is there needs to be a prompt added with that answer.
