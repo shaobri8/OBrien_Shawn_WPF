62eb403298cd2a103c0520a0959e554b9c7dca85
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 17:29:32 -0400

    1st project
    added the total and average equation.

--- Week 2 Homework/js/script.js
+++ Week 2 Homework/js/script.js
@@ -14,4 +14,7 @@ var week4 = prompt("How much did you loose in week 4");
 //Now we need to add all of the information that was given together to get a sum.
 var sum = [week1, week2, week3, week4];
 //Now to get the total.
-var total = sum[0] +sum[1] +sum[2] +sum[3]; 
+var total = sum[0] +sum[1] +sum[2] +sum[3];
+// In order to find the average you will need to divide by the number of prompts that were given.
+var average = total/4;
+// In order for the person to see what the average is there needs to be a prompt added with that answer.
