fdb6cfa8ce816c8c5c62d298666e1621005ec8e4
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 17:57:28 -0400

    1st project
    tried to see if “sum” was the problem and renamed the variable  to “a”.
    Didn’t work.

--- Week 2 Homework/js/script.js
+++ Week 2 Homework/js/script.js
@@ -12,13 +12,13 @@ var week3 = prompt("How much did you loose in week 3");
 var week4 = prompt("How much did you loose in week 4");
 
 //Now we need to add all of the information that was given together to get a sum.
-var sum = [week1, week2, week3, week4];
+var a = [week1, week2, week3, week4];
 // test to see if the answer is correct.
-console.log(sum);
+console.log(a);
 //Now to get the total.
-var total = sum[0] + sum[1] + sum[2] + sum[3];
+var total = a[0] + a[1] + a[2] + a[3];
 //Test to see if this answer is right.
-console.log(total); 
+console.log(total);
 // In order to find the average you will need to divide by the number of prompts that were given.
 var average = total / 4;
 // In order for the person to see what the average is there needs to be a prompt added with that answer.
