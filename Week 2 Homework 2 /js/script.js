068b59709060487ad18a692d836c8dec7f046a4d
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 19:22:32 -0400

    2nd Project
    Completed what I will design for the project. Also completed the
    prompts.

--- Week 2 Homework 2 /js/script.js
+++ Week 2 Homework 2 /js/script.js
@@ -4,3 +4,11 @@
  */
 //alert("Testing 1,2,3");
 
+// my current job is with physical security so. create a js file that will determine the perimeter
+// of a fence and how many lights will be needed.
+
+// The first thing that needs to be done is finding out what the parameters are.
+var side1 = prompt("What is the length of the north side of the property?");
+var side2 = prompt("What is the length of the south side of the property?");
+var side3 = prompt("What is the length of the east side of the property?");
+var side4 = prompt("What is the length of the west side of the property?");
