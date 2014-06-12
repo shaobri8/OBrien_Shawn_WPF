f8c77530dca2780528d2d5db14a11714117bb28f
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 16:28:13 -0400

    Using Prompts and alerts.
    Created prompts and alerts for this exercise

---
+++ AssociatingJS/exercises /js/script.js
@@ -0,0 +1,4 @@
+var width = prompt("we are calculating the area of a triangle.\nPlease enter the width");
+var height = prompt( "please enter the height");
+var result = width * height;
+console.log(result);
