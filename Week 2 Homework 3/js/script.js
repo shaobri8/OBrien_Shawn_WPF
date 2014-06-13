3720f309008fea18feb857bdce35bc6422e45aad
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 22:03:20 -0400

    Project 3
    Made it work, Now just need to fix some bugs like spacing and adding
    symbols.

--- Week 2 Homework 3/js/script.js
+++ Week 2 Homework 3/js/script.js
@@ -7,15 +7,15 @@
 //This is going to be kind of fun. Lets start with getting some information.
 
 alert("I AM GOING TO PREDICT YOUR FUTURE!!!");
-var dog = prompt("what is your dog\'s name?");
+var dog = prompt("What is your dog\'s name?");
 var fb = prompt("How many FaceBook friends do you have?");
 var wh = prompt("What is your favorite color?");
 var car = prompt("How many cars do you own?");
 // Now I need to make some changes to the numbers that will be input. So they will not stay the same.
-fb =- 10;
-car =+ 1;
+fb -= 10;
+car += 1;
 
 alert("Your will buy a cat and name it " + dog);
-alert("You will find " + fb + " on the ground");
-alert("You will buy a " + wh  + "house");
-alert("You will own " + car + "cars");
+alert("You will find $" + fb + " on the ground");
+alert("You will buy a " + wh  + " house");
+alert("You will own " + car + " cars");
