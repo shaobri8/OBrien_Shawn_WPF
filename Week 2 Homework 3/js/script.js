519ac0fc2e06a7f58300b74c72feb95db6ebac57
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 22:11:57 -0400

    Project 3
    Works great all bugs are fixed.

--- Week 2 Homework 3/js/script.js
+++ Week 2 Homework 3/js/script.js
@@ -7,15 +7,20 @@
 //This is going to be kind of fun. Lets start with getting some information.
 
 alert("I AM GOING TO PREDICT YOUR FUTURE!!!");
+
+//Now I will find out some information from the user.
+
 var dog = prompt("What is your dog\'s name?");
 var fb = prompt("How many FaceBook friends do you have?");
 var wh = prompt("What is your favorite color?");
 var car = prompt("How many cars do you own?");
+
 // Now I need to make some changes to the numbers that will be input. So they will not stay the same.
 fb -= 10;
 car += 1;
-
-alert("Your will buy a cat and name it " + dog);
+//Here is the formula I came up with to predict the users future.
+alert("You will buy a cat and name it " + dog);
 alert("You will find $" + fb + " on the ground");
 alert("You will buy a " + wh  + " house");
 alert("You will own " + car + " cars");
+// It has been checked and it works! 
