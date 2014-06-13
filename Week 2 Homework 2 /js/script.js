0d8477aab921bc90e66d365cc220d4d8e08d05ab
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 20:27:12 -0400

    Project 2
    Completed the process. Need to check the math.

--- Week 2 Homework 2 /js/script.js
+++ Week 2 Homework 2 /js/script.js
@@ -4,7 +4,7 @@
  */
 //alert("Testing 1,2,3");
 
-// my current job is with physical security so. create a js file that will determine the perimeter
+// my current job is with physical security so. I will Create a js file that is going determine the perimeter
 // of a fence and how many lights will be needed.
 
 // The first thing that needs to be done is finding out what the parameters are.
@@ -12,3 +12,15 @@ var side1 = prompt("What is the length of the north side of the property?");
 var side2 = prompt("What is the length of the south side of the property?");
 var side3 = prompt("What is the length of the east side of the property?");
 var side4 = prompt("What is the length of the west side of the property?");
+
+//now that we have the prompt inputs, we need to add them together to find the perimeter.
+var per = (Number(side1) + (Number(side2) + (Number(side3) +(Number(side4)))));
+// check the math.
+console.log(per);
+// So we now know the perimeter, we need to find how many lights we need. There is a given of 1 lamp post every 20 feet.
+var light = per/20;
+
+// The only thing we have left to do is add in an alert to let us know the answer.
+
+alert(light);
+
