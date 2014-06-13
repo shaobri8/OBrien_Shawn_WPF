ebd2bbdde132a5eacdb52adf16e874d1c2213804
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-12 21:44:27 -0400

    Project 3
    Decided on what I will do the project on and completed the first
    sequence.

--- Week 2 Homework 3/js/script.js
+++ Week 2 Homework 3/js/script.js
@@ -4,3 +4,11 @@
  */
 //alert("Testing 1,2,3");
 
+//This is going to be kind of fun. Lets start with getting some information.
+
+alert("I AM GOING TO PREDICT YOUR FUTURE!!!")
+var dog = prompt("what is your dog\'s name?");
+var fb = prompt("How many FaceBook friends do you have?");
+var wh = prompt("What is your favorite color?");
+var car = prompt("How many cars do you own?");
+
