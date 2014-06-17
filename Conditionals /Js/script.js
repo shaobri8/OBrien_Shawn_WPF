78c29da2eb74924e76b4af2b97c32dda1100f158
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-17 11:46:45 -0400

    Conditionals
    finished the exercise

--- Conditionals /Js/script.js
+++ Conditionals /Js/script.js
@@ -4,12 +4,12 @@
  */
 //alert("Testing 1,2,3");
 
-var budget = 300;
+var budget = 100;
 var iPhonePrice = 199.99;
-var payCheck = 200;
+var wonLottery = true;
 
 //if the price of the phone is less than the budget AND if our paycheck is over 300
-if(iPhonePrice < 300 && payCheck < 300){
+if(iPhonePrice < 300 || wonLottery === true){
    console.log ("You get a new phone!")
 }else{
     console.log("No phone for you!");
