e57a85f54045ed6b4736b6e6fc4651a622fa09dc
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-17 10:24:50 -0400

    Conditionals
    First examples are complete.

--- Conditionals /Js/script.js
+++ Conditionals /Js/script.js
@@ -3,3 +3,11 @@
  Assignment: Conditionals 17 June 2014
  */
 //alert("Testing 1,2,3");
+var oldEnough = false;
+// If the child is old enough print to the console "You can ride!"
+
+if(oldEnough){
+    //code preformed if condition is true.
+    console.log("You can ride the coaster!");
+}
+console.log("What comes after?");
