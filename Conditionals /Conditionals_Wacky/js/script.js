aedab265e6cbc8a58e347d4b2b8a87f8d2e6b15a
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 19:03:20 -0400

    Conditional_Wacky
    Completed the code and tested it.

--- Conditionals /Conditionals_Wacky/js/script.js
+++ Conditionals /Conditionals_Wacky/js/script.js
@@ -8,6 +8,9 @@ Assignment: Conditionals_Wacky
  //Here is the last conditional. I want to have a little fun with this one so I am going to make it into a game
  //called, guess what number I'm thinking of.
 
- var number = prompt("Guess what number I am thinking of?");
+ var number = prompt("Guess what number I am thinking of between 1-10?");
  if(number === ""){prompt("Seriously pick a number. You might do better than you think! ")}
- 
+
+ if(number<=5){prompt("No but your close guess again!")}
+var guess = prompt(" Not quite, this time pick between 5 and 10");
+ if(guess= 7 - 10){prompt("That's right!!!!! ")}
