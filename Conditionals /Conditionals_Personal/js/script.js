429426f13f61123ac7ef58227117714d8d3c4016
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 16:59:32 -0400

    Conditional_Personal
    Validated the first input

--- Conditionals /Conditionals_Personal/js/script.js
+++ Conditionals /Conditionals_Personal/js/script.js
@@ -8,6 +8,10 @@ Assignment: Conditional_Personal
  //I am going to create a condition that will let me know if I can go flying my rc plane today.
 
 var temp = prompt("What is the temperature?");
+
+if(temp===""){prompt("Please don't leave blank")}
+
+
 var wind = prompt("How windy is it today?");
 var weather = prompt("Will it most likely rain today?");
 
@@ -17,4 +21,6 @@ if(temp <=80 && temp >=40 && wind <= 10 && weather === "No"){console.log ("Yes y
 
 }else{console.log("No I wouldn't chance it.")}
 
-//Tested the code and it works. Great. Now on to the validation. 
+//Tested the code and it works. Great. Now on to the validation.
+ // We are going to need to validate to make sure numbers are put in to the interface and make sure people don't leave it
+ //blank.
