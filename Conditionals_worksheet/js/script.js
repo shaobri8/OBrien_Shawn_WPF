661a4ff762851b2f289a2f616a3e2123622dc667
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 10:01:38 -0400

    Completed variables
    Completed prompts to ask the user what is the eft. cap and gauge of
    their car.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -5,4 +5,12 @@ Assignment: Conditional_worksheet
  */
 //alert("Testing 1,2,3");
 
+ // The first thing that we need to do is figure out the variables.
+
+ var eff = prompt(" What is your cars mpg rating? ");
+ var gauge = prompt(" What is your current gas gauge reading? ");
+ var cap = prompt(" What is the capacity of the gas tank? ");
+
+ // Now that we have the var figured out, we need to input a solution to figure out if we can make it.
+
  
