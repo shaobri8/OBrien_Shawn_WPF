787cea433baa249a2530730dfb1b066d51f7fb3e
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 18:32:21 -0400

    Conditional_Industry
    Completed writing the code for the watch bill condition. Have not
    tested it yet.

--- Conditionals /Conditionals_Industry/js/script.js
+++ Conditionals /Conditionals_Industry/js/script.js
@@ -8,9 +8,13 @@ Assignment: Conditionals_Industry
  //for the industry conditionals I am going to create a conditional for writing a watch bill.
  // there are a few variables that need to be defined first.
 
- var hours = prompt("How may hours have they worked this week?");
- if(isNaN(hours)){prompt("Please enter a number value.")}
- var timeOff = prompt("It this their normal day off?");
- if(timeOff===""){prompt("Please do not leave blank")}
+ var hours = prompt("How may hours have they worked this week?"); // here is the variable
+ if(isNaN(hours)){prompt("Please enter a number value.")} //here the variable is validated
+ var timeOff = prompt("It this their normal day off?"); // here is the variable
+ if(timeOff===""){prompt("Please do not leave blank")} //here the variable is validated
 
  // I will write this statement in the required ternary style.
+ hours = (hours<40 && timeOff === "yes") ?"They will be able to work" : "No, you have to find someone else.";
+ console.log(hours);
+
+ 
