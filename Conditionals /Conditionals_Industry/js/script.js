266ab221a666d4c22f18459dc3d98b7dff34923d
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 18:24:32 -0400

    Conditional_Industry
    Completed validating the inputs for the variables.

--- Conditionals /Conditionals_Industry/js/script.js
+++ Conditionals /Conditionals_Industry/js/script.js
@@ -9,4 +9,8 @@ Assignment: Conditionals_Industry
  // there are a few variables that need to be defined first.
 
  var hours = prompt("How may hours have they worked this week?");
+ if(isNaN(hours)){prompt("Please enter a number value.")}
  var timeOff = prompt("It this their normal day off?");
+ if(timeOff===""){prompt("Please do not leave blank")}
+
+ // I will write this statement in the required ternary style.
