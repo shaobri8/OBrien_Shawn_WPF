c982dc87049e5ef97e60c510dbe39cb420ffc9d6
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 18:43:54 -0400

    Conditonal_Wacky
    Started on what I wanted this to be about. “Guess what number I am
    thinking of”

--- Conditionals /Conditionals_Wacky/js/script.js
+++ Conditionals /Conditionals_Wacky/js/script.js
@@ -4,3 +4,6 @@ WPF Section 101
 Assignment: Conditionals_Wacky
  */
 //alert("Testing 1,2,3");
+
+ //Here is the last conditional. I want to have a little fun with this one so I am going to make it into a game
+ //called, guess what number I'm thinking of.
