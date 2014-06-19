3207a35d48855ec30413cc892aff33c35b99df76
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 18:19:47 -0400

    Conditional_Industry
    Completed the variables

--- Conditionals /Conditionals_Industry/js/script.js
+++ Conditionals /Conditionals_Industry/js/script.js
@@ -4,3 +4,9 @@ WPF Section 101
 Assignment: Conditionals_Industry
  */
 //alert("Testing 1,2,3");
+
+ //for the industry conditionals I am going to create a conditional for writing a watch bill.
+ // there are a few variables that need to be defined first.
+
+ var hours = prompt("How may hours have they worked this week?");
+ var timeOff = prompt("It this their normal day off?");
