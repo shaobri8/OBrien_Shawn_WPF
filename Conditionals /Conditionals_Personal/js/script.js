1ea666c048e76af0856fe2e9ae97db2f1e82e71c
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 16:21:40 -0400

    Conditional_Personal
    Came up with the conditional and created the variables

--- Conditionals /Conditionals_Personal/js/script.js
+++ Conditionals /Conditionals_Personal/js/script.js
@@ -5,4 +5,8 @@ Assignment: Conditional_Personal
  */
 //alert("Testing 1,2,3");
 
- 
+ //I am going to create a condition that will let me know if I can go flying my rc plane today.
+
+var temp = prompt("What is the temperature?");
+var wind = prompt("How windy is it today?");
+var weather = prompt("Will it most likely rain today?");
