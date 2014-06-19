485269e39bb3c709b0d1803ef64b9835c95420ff
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 13:38:13 -0400

    Conditionals Worksheet.
    Completed, Note to self don’t forget about all of the tools available
    to you.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -7,16 +7,33 @@ Assignment: Conditional_worksheet
 
  // The first thing that we need to do is figure out the variables.
 
- var eff = prompt(" What is your cars mpg rating? ");
- var gauge = prompt(" At what percent is your tank gauge reading? ");
- var cap = prompt(" What is the capacity of the gas tank? ");
+ //var eff = prompt(" What is your cars mpg rating? ");
+ //var gauge = prompt(" At what percent is your tank gauge reading? ");
+ //var cap = prompt(" What is the capacity of the gas tank? ");
 
  // Now that we have the var figured out, we need to input a solution to figure out if we can make it.
 
- if(eff>20 && gauge >= 75 && cap >= 20){prompt("Yes, you can make it without stopping for gas!")
+ //if(eff>20 && gauge >= 75 && cap >= 20){prompt("Yes, you can make it without stopping for gas!")
 
 
- }else {prompt ("You don't have enough gas in your tank, better get gas now while you can!");
+ //}else {prompt ("You don't have enough gas in your tank, better get gas now while you can!");}
 
- }
- // I have made a formula to figure out if we can make it or not, however this formula does not include math. 
+
+ // I have made a formula to figure out if we can make it or not, however this formula does not include math.
+
+
+
+var percent = prompt(" What is the grade percent? ");
+// Here we used an interface to find our variable.
+
+ // we will use this variable to find out the grade below.
+if(percent >= 90){console.log ("You have earned a A in this class!")}
+if(percent >= 80 && percent<= 89){console.log ("You have earned a B in this class!")}
+if(percent >= 73 && percent<= 79){console.log ("You have earned a C in this class!")}
+if(percent >= 70 && percent<=72){console.log ("You have earned a D in this class!")}
+if(percent<= 69){console.log ("You have earned a F in this class!")}
+// code as been tested and works properly. 
+
+
+
+
