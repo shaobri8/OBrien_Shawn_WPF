77080772d1add6290b9805dc30cd24c46a60559c
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-19 13:55:54 -0400

    Conditionals worksheet.
    Completed everything up to writing the code.

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -27,12 +27,52 @@ var percent = prompt(" What is the grade percent? ");
 // Here we used an interface to find our variable.
 
  // we will use this variable to find out the grade below.
-if(percent >= 90){console.log ("You have earned a A in this class!")}
-if(percent >= 80 && percent<= 89){console.log ("You have earned a B in this class!")}
-if(percent >= 73 && percent<= 79){console.log ("You have earned a C in this class!")}
-if(percent >= 70 && percent<=72){console.log ("You have earned a D in this class!")}
-if(percent<= 69){console.log ("You have earned a F in this class!")}
-// code as been tested and works properly. 
+//if(percent >= 90){console.log ("You have earned a A in this class!")}
+//if(percent >= 80 && percent<= 89){console.log ("You have earned a B in this class!")}
+//if(percent >= 73 && percent<= 79){console.log ("You have earned a C in this class!")}
+//if(percent >= 70 && percent<=72){console.log ("You have earned a D in this class!")}
+//if(percent<= 69){console.log ("You have earned a F in this class!")}
+// code as been tested and works properly.
+
+
+
+
+// The next problem is finding out the price for a movie ticket. The first thing that we need to do is find out our
+ // variables.
+
+//The price of a ticket is $12, however this price varies by the time of showing and age of the customer, so these will
+ // be our variables.
+
+ // Now that our variables are set we need to create a prompt for the user.
+
+var old = prompt("How old are you?");
+var time = prompt("What time do you want to see the movie?");
+
+// We now need to write the statement to find out the price of the ticket. Lets say any movie before 5 pm gets the
+ //price of $7 and anyone who is over the age of 55 gets the price of $7 also. AND 7 is the lowest price available.
+
+ if(){
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
