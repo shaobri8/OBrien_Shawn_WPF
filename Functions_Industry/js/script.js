6ef8e5b27a46766672e453d187e4f76b491b11f3
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 19:54:35 -0400

    Functional Industry
    Completed with comments. I don’t know how to use the calculations in an
    if else statement.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,26 +6,27 @@ Assignment: Expressions
 //alert("Testing 1,2,3");
 
 
-// What I want to do now is find out if my web design business can afford new computers.
-
- // As always I will start with the variables.
 
 
+function monthlyAvg(week, amount, month){// Here is the named function followed by the variables.
+
+    var week= prompt("What is your weekly income?");// Variable week is needed for the user to input info.
+
+    while(isNaN(week) || week==="") {//Here I am validating the prompt with a while loop.
+        week = prompt("What is your weekly income?");
+    }
+    var amount= 52;// Another variable that is required to find out the average.
+
+    var month= 12;//The last variable that is needed to find out the average.
+
+var monthlyAvg = (week * amount / month);// This is the equation that adds it together.
+
+console.log (monthlyAvg);
+
+
+}
+monthlyAvg();// This is needed to complete the function. Known as the actuator.
 
 
 
 
-
- function Combine(weekIncome, weekYr, yr){
-
-     var weekIncome = prompt("What is your weekly income?");
-
-     var weekYr = 52; //because there is 52 weeks in a year.
-
-     var yr = 12; //because there are 12 months in a year.
-
-     var Combine = weekIncome * weekYr / yr;
-     
-     console.log (Combine);
-
- }
