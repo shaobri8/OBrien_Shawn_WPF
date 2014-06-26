56917cec557e23bac1becc22d317ee408d28b0da
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 19:56:39 -0400

    Functions Industry
    Completed some house keeping and insured my validating was working
    properly.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -8,24 +8,38 @@ Assignment: Expressions
 
 
 
-function monthlyAvg(week, amount, month){// Here is the named function followed by the variables.
+function monthlyAvg(week, amount, month){
+// Here is the named function followed by the variables.
 
-    var week= prompt("What is your weekly income?");// Variable week is needed for the user to input info.
 
-    while(isNaN(week) || week==="") {//Here I am validating the prompt with a while loop.
+    var week= prompt("What is your weekly income?");
+    // Variable week is needed for the user to input info.
+
+
+    while(isNaN(week) || week==="") {
+    //Here I am validating the prompt with a while loop.
         week = prompt("What is your weekly income?");
     }
-    var amount= 52;// Another variable that is required to find out the average.
 
-    var month= 12;//The last variable that is needed to find out the average.
 
-var monthlyAvg = (week * amount / month);// This is the equation that adds it together.
+    var amount= 52;
+    // Another variable that is required to find out the average.
+
+
+    var month= 12;
+    //The last variable that is needed to find out the average.
+
+
+var monthlyAvg = (week * amount / month);
+// This is the equation that adds it together.
+
 
 console.log (monthlyAvg);
 
 
 }
-monthlyAvg();// This is needed to complete the function. Known as the actuator.
+monthlyAvg();
+// This is needed to complete the function. Known as the actuator.
 
 
 
