dc18e083c27a9099e12f926ef0f3d30dfdf5c046
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 17:45:14 -0400

    Functions Industry
    I completed the first part of the equation. It has been tested and
    proven.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,3 +6,26 @@ Assignment: Expressions
 //alert("Testing 1,2,3");
 
 
+// What I want to do now is find out if my web design business can afford new computers.
+
+ // As always I will start with the variables.
+
+
+
+
+
+
+
+ function Combine(weekIncome, weekYr, yr){
+
+     var weekIncome = prompt("What is your weekly income?");
+
+     var weekYr = 52; //because there is 52 weeks in a year.
+
+     var yr = 12; //because there are 12 months in a year.
+
+     var Combine = weekIncome * weekYr / yr;
+     
+     console.log (Combine);
+
+ }
