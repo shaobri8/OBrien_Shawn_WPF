041eb6385f6727701c91b77b576c3c09891408af
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 13:17:38 -0400

    Functions 1st assignment.
    Fixed problem with code. Made a variable to return on the console.log.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -7,14 +7,13 @@ Assignment: Expressions
 
 
 
-var radius = 5; // Here I came up with a var for the radius. Seeing how we need to find the circumference.
- // that is pie(3.14) times the radius.
-
-var pie = 3.14;// here is pie which I made my second variable.
+var complete = cir(5, 3.14);
 
 function cir(radius, pie){ // Here are the "Containers" that are used for the variables.
-    var cir = radius * pie;// to make the function I needed to enter the calculation.
+    var cir = radius * pie;
+    return cir; // to make the function I needed to enter the calculation.
 
 
 }
 
+console.log (complete);
