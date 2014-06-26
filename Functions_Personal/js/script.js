7a3ee99d5ad10dd824919f5dd265cbff7e36cd77
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 15:10:46 -0400

    Function Personal
    Completed set up and added variables with function.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,6 +1,25 @@
 
  /* Shawn O'Brien
 WPF Section 101
-Assignment: Expressions
+Assignment: Functions Date 06/26/14
  */
 //alert("Testing 1,2,3");
+
+ //When running a rc helicopter it is useful to know know how many watts of power you are using. This code is gather
+ //info from prompts and calculate what the wattage will be.
+
+ //The first thing I want to do is create a variable to use for the function to return.
+
+ var totalWatts = watts(20, 12); // Here is the info that will be put in the "Container" that are in the function.
+
+ function watts(amps, volts){
+     watts = amps * volts;
+     return (watts);
+
+
+
+ }
+
+
+
+
