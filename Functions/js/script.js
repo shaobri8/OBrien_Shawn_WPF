8a47f81a8f973668a642c0610cbf2ac546961d30
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 11:27:52 -0400

    Functions
    Completed 4th lesson parameters .

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -5,7 +5,7 @@ Assignment: Functions
  */
 //alert("Testing 1,2,3");
 
- function outptMsg(){
+ /*function outptMsg(){
 
      console.log ("Hello World");
  }
@@ -20,3 +20,23 @@ var width = 5;
  }
  calcArea();
  console.log(width);
+*/
+
+/*calcArea(30, 20);
+
+ function calcArea(w, h){ //w = 30 and height = 20
+  var area = w * h;
+     console.log (area);
+
+ }
+
+*/
+
+ function dogYear (age){// parameter 
+     var dogYear = age * 7;
+     console.log ("Sparky is " + dogYear + " years old.");
+
+ }
+dogYear(4); // arguments
+dogYear(5);
+
