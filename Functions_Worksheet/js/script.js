168334ca041a8b57c13ad1587f94024a9547f74a
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 13:12:23 -0400

    Functions first assignment.
    Made some mistakes with the “return” function. I need to go back and
    fix it.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -5,13 +5,16 @@ Assignment: Expressions
  */
 //alert("Testing 1,2,3");
 
-var radius = 5;
-var pie = 3.14;
 
-function cir(radius, pie){
-    var cir = radius * pie;
+
+var radius = 5; // Here I came up with a var for the radius. Seeing how we need to find the circumference.
+ // that is pie(3.14) times the radius.
+
+var pie = 3.14;// here is pie which I made my second variable.
+
+function cir(radius, pie){ // Here are the "Containers" that are used for the variables.
+    var cir = radius * pie;// to make the function I needed to enter the calculation.
 
 
 }
- var cir = radius * pie;
- console.log("The circumference of the circle is " + cir);
+
