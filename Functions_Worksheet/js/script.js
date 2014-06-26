a41f3004332a964df7c57ebebd0af3cb109e5d55
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 13:46:12 -0400

    Assignment 2, bee stings.
    Completed the code and works properly.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -6,6 +6,8 @@ Assignment: Expressions
 //alert("Testing 1,2,3");
 
 
+//Circumference!
+
 //Because we need find the circumference which is pie * the radius, I made a variable for each.
 
 var complete = cir(5, 3.14); // Here I gave the parameters of the equation "pie and radius". I also made the variable
@@ -21,5 +23,17 @@ function cir(radius, pie){ // Here are the "Containers" that are used for the va
 console.log ("The circumference of the circle is " + complete); // Here is the outcome of the equation. It will be
  //printed to the console as shown.
 
- 
+ // Stung!
+ // We need to find out how many bee stings it takes to kill an animal.
+ //I am going to start out by making my variable and setting my parameters.
 
+ var total = beeSting(8.666666667, 180 ); //I entered the amount of bee stings required per pound and the weight of a
+ //victim.
+
+ function beeSting(sting, pounds ){// the function requires that I put in the containers.
+     var beeSting = sting * pounds;// Here I am telling the factory what to do with what's in those containers.
+     return beeSting;//Now I am returning the answer so that we can print it to the console. 
+
+ }
+
+console.log (total);
