f1dee94b89579394046b80469bf26e6512cbb9c2
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 12:59:35 -0400

    Completed the first assignment.
    Worksheet assignment for finding the circumference of a circle.

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,17 @@
+
+ /* Shawn O'Brien
+WPF Section 101
+Assignment: Expressions
+ */
+//alert("Testing 1,2,3");
+
+var radius = 5;
+var pie = 3.14;
+
+function cir(radius, pie){
+    var cir = radius * pie;
+
+
+}
+ var cir = radius * pie;
+ console.log("The circumference of the circle is " + cir);
