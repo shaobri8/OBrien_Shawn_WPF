dc87ad97e90db233996cf4788d78997fb2501dff
shaobri8 <shawnobrien@Shawns-MacBook-Pro.local>
2014-06-26 10:54:38 -0400

    Functions
    Completed first exercise.

--- Functions/js/script.js
+++ Functions/js/script.js
@@ -3,4 +3,18 @@
 WPF Section 101
 Assignment: Functions
  */
-alert("Testing 1,2,3");
+//alert("Testing 1,2,3");
+
+ function outptMsg(){
+
+     console.log ("Hello World");
+ }
+
+ function calcArea(){
+
+     var width = 20;
+     var height = 30;
+     var area = width * height;
+     console.log(area);
+
+ }
